861d007ebd8f8b97cec9fe44dcbfaef595e6a09e
PRIYA <priikum942@gmail.com>
2026-08-19 05:20:08 +0530

    Time: 59 ms (35.31%), Space: 80 MB (23.89%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+vector<vector<string>>ans;
+    void solve(string &s,int n,int ind,vector<vector<string>>&ans,vector<string>&temp)
+    {
+        //base-case
+        if(ind == n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int k=ind;k<n;k++)
+        {
+            int i = ind;
+            int j = k;
+            bool val = true;
+
+
+            while(i<=j)
+            {
+                if(s[i]!=s[j])
+                {
+                    val = false;
+                    break;
+                }
+                i++;
+                j--;
+            }
+
+            if(val)
+            {
+                 temp.push_back(s.substr(ind, k - ind + 1));
+                solve(s,n,k+1,ans,temp);
+                temp.pop_back();
+
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+       int n = s.size();
+       vector<string>temp;
+       
+       solve(s,n,0,ans,temp); 
+       return ans;
+    }
+};
