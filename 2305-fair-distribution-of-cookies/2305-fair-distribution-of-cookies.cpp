905f140b2710f3b3d8da5ed63fe3f65643531529
PRIYA <priikum942@gmail.com>
2026-08-19 07:20:32 +0530

    Time: 832 ms (18.55%), Space: 9 MB (73.52%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -3,10 +3,10 @@ public:
     int maxi = INT_MIN;
     int mini = INT_MAX;
 
-    void solve(vector<int>& cookies, int k,int n,int ind,int child,vector<int>&temp)
+    void solve(vector<int>& cookies, int k,int n,int ind,vector<int>&temp)
     {
         //if cookies are exhaust 
-        if(ind>=n)
+        if(ind==n)
         {
            maxi = *max_element(temp.begin(), temp.end());
            mini = min(maxi,mini);
@@ -16,14 +16,14 @@ public:
         for(int i=0;i<k;i++)
         {
             temp[i]+=cookies[ind];
-            solve(cookies,k,n,ind+1,i,temp);
+            solve(cookies,k,n,ind+1,temp);
             temp[i]-=cookies[ind];
         }
     }
     int distributeCookies(vector<int>& cookies, int k) {
         int n = cookies.size();
         vector<int>temp(k,0);
-        solve(cookies,k,n,0,0,temp);
+        solve(cookies,k,n,0,temp);
 
         return mini;
     }
