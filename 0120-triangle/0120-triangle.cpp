fcfdf529c050dcd3f49c9a2c4e45d0f325268ac8
PRIYA <priikum942@gmail.com>
2026-08-20 06:01:53 +0530

    Time: 0 ms (100%), Space: 13.5 MB (26.17%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,58 +1,49 @@
 class Solution {
-public:
-    // int solve(vector<vector<int>>& triangle, int row, int col_ind,
-    //           vector<vector<int>>& dp) {
-    //     if (row == 0) {
-    //         return triangle[0][0];
+public: 
+    // int solve(vector<vector<int>>& triangle,int r,int c,vector<vector<int>>&dp)
+    // {
+    //     int n = triangle.size();
+    //     if(r >=n || c >= r+1)
+    //     {
+    //         return 0;
     //     }
-    //     if (row < 0 || col_ind < 0) {
-    //         return 1e9;
+    //     if(r == n-1)
+    //     {
+    //         return triangle[r][c];
     //     }
-    //     if (col_ind > row) {
-    //         return 1e9;
+    //     if(dp[r][c]!=1e9)
+    //     {
+    //         return dp[r][c];
     //     }
-    //     if (dp[row][col_ind] != -1) {
-    //         return dp[row][col_ind];
-    //     }
-    //     int up = triangle[row][col_ind] + solve(triangle, row - 1, col_ind, dp);
-    //     int left =
-    //         triangle[row][col_ind] + solve(triangle, row - 1, col_ind - 1, dp);
+    //     int down = triangle[r][c] + solve(triangle,r+1,c,dp);
+    //     int right = triangle[r][c] +  solve(triangle,r+1,c+1,dp);
 
-    //     return dp[row][col_ind] = min(up, left);
+    //     return dp[r][c] = min(down,right);
     // }
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        int last_row = triangle[n - 1].size();
-        int mini = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        dp[0][0] = triangle[0][0];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int k=0;k<n;k++)
+        {
+            dp[n-1][k] = triangle[n-1][k];
+        }
+        //here we grom from down to up 
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
 
-                int up = triangle[i][j] + dp[i - 1][j];
-                int left = triangle[i][j];
-                if(j>0)
+                int right = triangle[i][j];
+                if(j<n-1)
                 {
-                    left += dp[i - 1][j - 1];
+                    right += dp[i+1][j+1];
                 }
-        
-                if (j == 0) {
-                    // at 0 only one direction available
-                    dp[i][j] = up;
-                }
-                else if (j == i) {
-                    // same here
-                    dp[i][j] = left;
-                } else {
-                    dp[i][j] = min(up, left);
-                }
+
+                dp[i][j] = min(down,right);
             }
         }
-        for (int i = 0; i < last_row; i++) {
-            int ans = dp[n-1][i];
-            mini = min(mini, ans);
-        }
-
-        return mini;
+        return dp[0][0];
     }
 };
