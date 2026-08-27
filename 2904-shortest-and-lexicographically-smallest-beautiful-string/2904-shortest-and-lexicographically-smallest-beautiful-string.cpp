6caaadcd9237c7c91b151499df183376fe72db89
PRIYA <priikum942@gmail.com>
2026-08-27 05:51:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (82.61%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,33 +2,59 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         string ans = "";
+        int i=0;
+        int j= 0;
         int n = s.size();
+        int cnt = 0;
+        if(n == 0)
+        {
 
-        for (int i = 0; i < n; i++) {
+        } 
 
-            int oneCnt = 0;
-            string cur = "";
-
-            for (int j = i; j < n; j++) {
-
-                cur += s[j];
-
-                if (s[j] == '1')
-                    oneCnt++;
-
-                // More than k ones can never become valid again
-                if (oneCnt > k)
-                    break;
-
-                if (oneCnt == k) {
-
-                    if (ans == "" ||
-                        cur.size() < ans.size() ||
-                        (cur.size() == ans.size() && cur < ans)) {
-
-                        ans = cur;
-                    }
+        while(i<n && j<n)
+        {
+            while(i<n && s[i]=='0')
+            {
+                i++;
+            }
+            if(j<i)
+            {
+                j = i;
+            }
+            while(j<n && cnt < k )
+            {
+                if(s[j]=='1')
+                {
+                    cnt++;
                 }
+                j++;
+            }
+
+            if(cnt == k)
+            {
+                int len = j - i;
+                string curr = s.substr(i, j - i);
+                if(ans == "")
+                {
+                    ans = curr;
+                }
+                else if(len < ans.size())
+                {
+                    ans = curr;
+                }
+                else if(len == ans.size() && curr < ans)
+                {
+                    ans = curr;
+                }
+                if (s[i] == '1') {
+                    cnt--;
+                }
+
+                i++;
+            }
+            else {
+                // Not enough 1s remaining
+                break;
             }
         }
 
